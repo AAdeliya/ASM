205e44c0b2b85ad55a36d861120eb7e16401760d
firstPush <adel.sdet2@gmail.com>
2024-04-04 10:49:38 -0500

    ForLoops

---
+++ ForLoops.java
@@ -0,0 +1,20 @@
+public class ForLoops {
+    public static void main(String[] args) {
+        System.out.println("I love dogs!");
+        //how many times you want to run your code
+        for(int i=0; i<10; i++){
+            System.out.println("I love dogs!");
+            //10- how many times you want to run a program
+        }
+        for(int i=0; i<4; i++){
+            System.out.println(i);
+        }
+        int[] grades= {98, 100, 83, 90, 93};
+        for(int i=0; i<grades.length; i++){
+            System.out.println(grades[i]);
+
+        }
+
+    }
+    
+}

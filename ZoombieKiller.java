d01df81b4133628344875a8d313ec96a9c06a358
firstPush <adel.sdet2@gmail.com>
2024-04-04 10:30:58 -0500

    Arrays/Zoombies

---
+++ ZoombieKiller.java
@@ -0,0 +1,22 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class ZoombieKiller {
+    public static void main(String [] args){
+        System.out.println("Welcome to our killer prograam!!!");
+        String backpack[]= {"Shotgun", "Assault", "Sniper"};
+        String zoombies[]= {"close-range", "Mid-range zoombie", "Long-range zoombie"};
+        System.out.println(backpack[0]);
+        System.out.println(backpack[1]);
+        System.out.println(backpack[2]);
+        System.out.println(zoombies[0]);
+        System.out.println(zoombies[1]);
+        System.out.println(zoombies[2]);
+        System.out.println(backpack[2]);
+        System.out.println(backpack[2]);
+        System.out.println(backpack[2]);
+        System.out.println(backpack[0]);
+        int numbersZoombiesHATE[]= {4, 90, 70, 12, 4, 561, 1};
+        System.out.println(numbersZoombiesHATE[5]);
+    }
+    
+}

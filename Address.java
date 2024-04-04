87b4ffe08aa9f015193b0b9707727277cc3caf4e
firstPush <adel.sdet2@gmail.com>
2024-04-03 23:11:33 -0500

    second

---
+++ Address.java
@@ -0,0 +1,7 @@
+public class Address {
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    
+}
---
+++ Person.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+
+public class Person{
+
+private String firstName;
+private String lastName;
+private LocalDate dob;
+private Address address;
+
+public Person (String firstName, String lastNme, LocalDate dob){
+this.firstName=firstName;
+this.lastName=lastNme;
+this.dob=dob;
+}
+public String getFirstName(){
+    return firstName;
+}
+public void setFirstName(String firstName){
+    this.firstName=firstName;
+}
+public String getLastName(){
+    return lastName;
+}
+public void setLastName(String lastName) {
+    this.lastName = lastName;
+}
+public LocalDate getDob() {
+    return dob;
+}
+public void setDob(LocalDate dob) {
+    this.dob = dob;
+}
+public Address getAddress() {
+    return address;
+}
+public void setAddress(Address address) {
+    this.address = address;
+}
+
+}
